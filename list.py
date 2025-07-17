8bccdda617e89a863020ad8d02b89f70c624aad8
sanjaycheruku <cherukusanjay07@gmail.com>
2025-07-17 23:17:05 +0530

    list

---
+++ list.py
@@ -0,0 +1,21 @@
+#list examples
+#v=[2,3,4,23,"sanju",44,332]
+#print(v)
+'''
+append
+extend
+count
+remove 
+pop 
+index
+'''
+v=[2,3,2,4.4,23,'max',44,2,332]
+#append=(44)
+#extend=(44,33,55,'max')
+#print(v.count(2))
+
+#v.remove(2)
+#v.pop(3)
+#print(v.index(4.4))
+# call two elements in a time
+print(v[2] , v[3])
