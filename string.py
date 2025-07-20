b17c742b68a5de1f61fe7580eca9c2e8279b8739
sanjaycheruku <cherukusanjay07@gmail.com>
2025-07-20 22:29:12 +0530

    String

---
+++ string.py
@@ -0,0 +1,11 @@
+# String is a collection of charaters 
+"""
+      types
+      single quote
+      double
+      thrible
+"""
+a='nvkb'
+b="kfkjke's"
+c=""" hverhjb"""
+#print(type(a),type(b),type(c))
