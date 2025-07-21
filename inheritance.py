ebff26e9c07d13c2887dc54c177d965c2e672796
sanjaycheruku <cherukusanjay07@gmail.com>
2025-07-21 20:51:03 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+"""
+types 
+single inheritance
+multi level
+multiple
+herarical          
+"""
+class a():
+    def feature1(self):
+        print('hi')
+a1=a() 
+a1.feature1()
+class b(a):
+    def feature2(self):
+        print('hi2')
+b1=b() 
+b1.feature2()
